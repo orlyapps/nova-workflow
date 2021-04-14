932aaa76978aa06589e293bd99e9632b37a260f5
Orlyapps <info@orlyapps.de>
2021-04-14 14:45:36 +0200

    php8 fix

--- src/Traits/HasWorkflow.php
+++ src/Traits/HasWorkflow.php
@@ -163,7 +163,10 @@ trait HasWorkflow
             $metadata = $metadataStore->getTransitionMetadata($transition);
 
             $policyName = \Str::camel('can_see_' . $transitionName);
-            $policyExists = method_exists($policy, $policyName);
+            $policyExists = false;
+            if ($policy) {
+                $policyExists = method_exists($policy, $policyName);
+            }
 
             // Default die actions anzeigen, nur wenn eine Policy existiert prüfen
             $canSee = true;
